ef9a0aaf23ccc3386d3028137f42b220bad4e2ce
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-03 14:07:42 +0300

    Add T17

---
+++ T17/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// точки (X, Y), причем X неравно нулю и Y неравно нулю, и
+// выдает номер четвертиплоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X = ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y = ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+if (xCoordinate > 0 && yCoordinate > 0)
+{
+    Console.WriteLine("Укаанные координаты соответствуют 1 четверти");
+}
+else if (xCoordinate < 0 && yCoordinate > 0)
+{
+    Console.WriteLine("Укаанные координаты соответствуют 2 четверти");
+}
+else if (xCoordinate < 0 && yCoordinate < 0)
+{
+    Console.WriteLine("Укаанные координаты соответствуют 3 четверти");
+}
+else if (xCoordinate > 0 && yCoordinate < 0)
+{
+    Console.WriteLine("Укаанные координаты соответствуют 4 четверти");
+}
+else Console.WriteLine("Введены некорректные координаты");
+// Console.WriteLine("Hello, World!");
