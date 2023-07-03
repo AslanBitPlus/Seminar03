d23e9be9b0d459e2f9c1d3a6e4229d4979828ca8
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-03 17:18:12 +0300

    Add T22

---
+++ T22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число
+// N и выдает таблицу квадратов чисел от 1 до N
+// пространстве
+// 5 -> 1, 4, 9, 25
+// 2 -> 1, 4
+
+// =======================
+Console.WriteLine("Введите целое положительное число:");
+Console.Write("N = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0) 
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.Write(count);
+        Console.WriteLine($"   {count * count}");
+        count ++;
+    }
+}
+else Console.WriteLine("Ошибка ввода");
+
+
+// Square(num);
