4a86bad0013722e7e0a2185ac87f3ae915e0552e
Aslan Agadadashev <aslan.pressa@yandex.ru>
2023-07-03 15:05:36 +0300

    Add T18

---
+++ T18/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диаппазон возможных координат точек в этой
+// четверти (X, Y)
+// =======================
+string Quarter(int x)
+{
+    string result = "Введен неправилный номер четверти";
+    if (x == 1 ) result = "X > 0, Y > 0";
+    else if (x == 2 ) result = "X < 0, Y > 0";
+    else if (x == 3) result = "X < 0, Y < 0";
+    else if (x == 4) result = "X > 0, Y < 0";
+    return result;
+}
+// =======================
+
+Console.Write("Введите номер четверти: ");
+int quorterNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Quarter(quorterNumber));
+
+
+
